02604f1e637f9f86137d93ce73a777d7bca50ba9
Julien BOYER <julien.boyer@sonovisiongroup.com>
2020-11-23 15:59:00 +0100

    ajout des fonctions getAllProduct orderProductById

---
+++ src/Products.js
@@ -0,0 +1,55 @@
+let fs = require('fs');
+
+module.exports = class Products {
+
+	constructor(path, callback){
+		this.path = path;
+		this.list = null;
+		this.read(callback);
+	}
+
+	getAllProducts(callback){
+		if(!callback) callback = ()=>{};
+		this.read((err)=>{
+			if(err) return callback(err);
+			console.log('Bienvenue. Voici les produits disponibles:');
+			this.list.forEach(product => {
+				console.log(`${product.id} - ${product.name} / ${product.EUR_price} / ${product.orders_counter}`);
+			});
+			if(callback) callback(null, this.list);
+		});
+	}
+	
+	orderProductById(id, callback){
+		if(!callback) callback = ()=>{};
+		this.read((err)=>{
+			if(err) return callback(err);
+			let product = this.list.find(p => p.id == id);
+			product.orders_counter ++;
+			console.log('Commande terminée. Voici votre fichier: '+product.file_link);
+			this.write(callback);
+		})
+	}
+
+	read(callback){
+		fs.readFile(this.path, 'utf8', (err, file) => {
+			if(err) {
+				return callback(err);
+			}
+			try {
+				this.list = JSON.parse(file);
+				callback(null, this);
+			} catch(e) {
+				console.error(e);
+				callback(e);
+			}
+		});
+	}
+
+	write(callback){
+		let str = JSON.stringify(this.list, null, 2);
+		fs.writeFile(this.path, str, (err) => {
+			callback(err);
+		});
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 let fs = require('fs');
+let path = require('path');
+var readline = require('readline');
+let Products = require('./Products.js')
 
-fs.readFile(`${__dirname}/products.json`, 'utf8', (err, file) => {
-	var products = JSON.parse(file);
-	console.log(products);
+var rl = readline.createInterface({input: process.stdin, output: process.stdout, terminal: false});
+
+new Products(path.join(__dirname,'products.json'), (err, products) => {
+	if(err) return console.error(err);
+	products.getAllProducts();
+	
+	rl.on('line', function(line){
+		console.log('I want product', line);
+		products.orderProductById(line.toString());
+	})
 });
